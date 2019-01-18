5d52289b96127f09722902aa3c906b3693ae773d
Horacio Fernandes <horacioalexandre.fernandes@gmail.com>
2019-01-18 19:11:39 -0200

    Enable text compression - gzip

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require("express")
 const path = require("path")
 const port = process.env.PORT || 8080
+const compression = require("compression")
 const app = express()
+
+app.use(compression())
 
 app.use(express.static(__dirname))
 app.use(express.static(path.join(__dirname, "build")))
-
-app.get("/", (req, res) =>
-  res.redirect(301, "https://github-portal-shawn.herokuapp.com/users")
-)
 
 app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"))
