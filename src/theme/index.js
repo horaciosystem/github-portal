c844fac888583dfed1e6d830cd8842e9cb9de074
Horacio Fernandes <horacioalexandre.fernandes@gmail.com>
2019-01-18 10:25:34 -0200

    Add theme files

---
+++ src/theme/index.js
@@ -0,0 +1,24 @@
+import defaultTheme from "reakit-theme-default"
+
+const theme = {
+  ...defaultTheme,
+  palette: {
+    ...defaultTheme.palette,
+    black: "#2C2C2C",
+    black10: "#e5e5e5",
+    text: "#2C2C2C",
+    text75: "#616161",
+    text50: "#959595",
+    text25: "#CACACA",
+    error: "#d32f2f"
+  },
+  pageWidth: "1200px",
+  spacing: "1.6rem",
+  fontWeight: {
+    regular: 400,
+    semibold: 600,
+    bold: 700
+  }
+}
+
+export default theme
