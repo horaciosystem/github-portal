a89c776d86cd89727ae0993871d1b5e3d425b431
Horacio Fernandes <horacioalexandre.fernandes@gmail.com>
2019-01-18 19:00:04 -0200

    Redirect to users page when accessing app root

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const app = express()
 app.use(express.static(__dirname))
 app.use(express.static(path.join(__dirname, "build")))
 
-app.get("/", (req, res) => res.redirect(301, "/users"))
+app.get("/", (req, res) =>
+  res.redirect(301, "https://github-portal-shawn.herokuapp.com//users")
+)
 
 app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"))
